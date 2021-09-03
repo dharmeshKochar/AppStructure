32e98ed5827f835422fa0be1e2ca98ea25196be6
dharmeshKochar <dharmesh.kochar@appinventiv.com>
2021-09-03 09:52:35 +0530

    feat: tabBar

--- AppStructure/ViewControllers/WelcomeVC.swift
+++ AppStructure/ViewControllers/WelcomeVC.swift
@@ -22,6 +22,12 @@ class WelcomeVC: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var tabBarView: UIView!
     @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var messageButton: UIButton!
+    @IBOutlet weak var galleryButton: UIButton!
+    @IBOutlet weak var menuBarView: UIView!
+    @IBOutlet weak var menuButtonStack: UIStackView!
     
     //MARK:- ViewLifeCycle
     override func viewDidLoad() {
@@ -45,15 +51,64 @@ class WelcomeVC: UIViewController {
              image = UIImage(named: "sample1")
              self.animationtableView.reloadData()
         })
+        createTabBar()
+        menuBarView.isHidden = true
+        menuButtonStack.layer.cornerRadius = 32
+        menuButtonStack.layer.masksToBounds = true
+    }
+    
+    func createTabBar(){
+        let path  = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: 0))
+        let width = (tabBarView.frame.width ) / 3 
+        path.addLine(to: CGPoint(x: width - 15  , y: 0))
+        path.addArc(withCenter:CGPoint(x: view.center.x - 15 , y: -15) , radius: (tabBarView.frame.width / 3)/2 , startAngle: -CGFloat.pi , endAngle: 0, clockwise: false)
+        path.addLine(to: CGPoint(x: 3*width , y: 0))
+        path.addLine(to: CGPoint(x: tabBarView.frame.width , y: tabBarView.frame.height))
+        path.addLine(to: CGPoint(x: 0 , y: tabBarView.frame.height ))
+        path.addLine(to: CGPoint(x: 0, y: 0))
+        let shapLayer = CAShapeLayer()
+        shapLayer.path = path.cgPath
+        shapLayer.cornerRadius = 20
+        shapLayer.fillColor = UIColor.white.cgColor
+        tabBarView.layer.insertSublayer(shapLayer, at: 0)
+        tabBarView.layer.cornerRadius = 20
+        tabBarView.clipsToBounds = true
+    }
+    
+    @IBAction func cancelButtonAction(_ sender: UIButton) {
+        cameraButton.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+        galleryButton.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+        messageButton.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+        cancelButton.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+        UIView.animate(withDuration: 0.3) { [self] in
+            menuButtonStack.transform = CGAffineTransform(scaleX: 0.5, y: 1)
+        } completion: { [self] _ in
+            plusButton.setImage(UIImage(systemName: "plus.circle.fill"), for: .normal)
+            UIView.animate(withDuration: 0.3) {
+                menuBarView.isHidden = true
+                plusButton.transform = CGAffineTransform(translationX: 0, y: 8)
+            }
+        }
     }
     
     @IBAction func plusButtonAction(_ sender: UIButton) {
-        UIView.animate(withDuration: 2) { [self] in
-            plusButton.center.y = plusButton.center.y - 32 }
-            completion: { [self] _ in
-                plusButton.center.y = plusButton.center.y - 32
-                plusButton.setImage(UIImage(systemName: "dot.circle"), for: .selected)
+        UIView.animate(withDuration: 0.5){
+            self.plusButton.transform = CGAffineTransform(translationX: 0, y: -42)
+        } completion: { [self] _ in
+            plusButton.imageEdgeInsets = UIEdgeInsets(top: 5,left: 5,bottom: 5,right: 5)
+            plusButton.setImage(UIImage(systemName: "circle.fill"), for: .normal)
+            
+            UIView.animate(withDuration: 0.3) {
+                menuBarView.isHidden = false
+                menuButtonStack.transform = CGAffineTransform(scaleX: 2.5, y: 1)
+            } completion: { _ in
+                cameraButton.setImage(UIImage(systemName: "camera"), for: .normal)
+                galleryButton.setImage(UIImage(systemName: "heart"), for: .normal)
+                messageButton.setImage(UIImage(systemName: "message"), for: .normal)
+                cancelButton.setImage(UIImage(systemName: "multiply"), for: .normal)
             }
+        }
     }
      
 }
